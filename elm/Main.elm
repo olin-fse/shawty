87b873d67a74637c1b533560cbcb9a5cbc0668da
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2017-10-08 18:51:13 -0400

    Post request working

---
+++ elm/Main.elm
@@ -0,0 +1,92 @@
+import Html exposing (Html, Attribute, div, input, button, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
+import Http
+import Json.Decode as Decode
+import Json.Encode exposing (..)
+import Debug exposing (log)
+
+main =
+    Html.program 
+        { init = init ""
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+-- MODEL
+
+type alias Model =
+    { userUrl : String
+    , generatedUrl: String
+    }
+
+init : String -> (Model, Cmd Msg)
+init userUrl = 
+    ( Model userUrl ""
+    , Cmd.none
+    )
+
+-- UPDATE
+
+type Msg
+    = Shorten
+    | ShowUrl (Result Http.Error String)
+    | Change String
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Shorten ->
+            (model, shortenUrl model.userUrl)
+
+        Change newContent ->
+            ({ model | userUrl = newContent }, Cmd.none)
+
+        ShowUrl (Ok newUrl) ->
+            (Model model.generatedUrl newUrl, Cmd.none)
+
+        ShowUrl (Err _) ->
+            (model, Cmd.none)
+
+
+-- HTTP
+
+shortenUrl : String -> Cmd Msg
+shortenUrl url =
+    let
+        request =
+            Http.post "/generate" (Http.jsonBody (encodeUserUrl url)) decodeGeneratedUrl
+    in
+        Http.send ShowUrl request
+
+encodeUserUrl : String -> Value
+encodeUserUrl url =
+    object
+        [ ("userUrl", string url) 
+        ]
+            
+decodeGeneratedUrl : Decode.Decoder String
+decodeGeneratedUrl =
+    Decode.at ["data", "generated_url"] Decode.string
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ input [ placeholder "Url to shorten", onInput Change ] []
+        , div [] [ text (model.userUrl) ]
+        , button [ onClick Shorten ] [ text "Shorten Url" ]
+        ]
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+
--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"html/template"
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"encoding/json"
+)
+
+type requestBody struct {
+	UserUrl string
+}
+
 func main() {
-	println("Hello!")
+	http.Handle("/", http.FileServer(http.Dir("./elm/static")))
+	http.HandleFunc("/generate", generate)
+
+	fmt.Println("Listening...")
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	lp := filepath.Join("elm", "static")
+	fp := filepath.Join("templates", "/index.html")
+
+	tmpl, _ := template.ParseFiles(lp, fp)
+	tmpl.ExecuteTemplate(w, "layout", nil)
+}
+
+func generate(w http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var body requestBody
+	err := decoder.Decode(&body)
+	if err != nil {
+		panic(err)
+	}
+	defer req.Body.Close()
+	fmt.Println(body.UserUrl)
 }
