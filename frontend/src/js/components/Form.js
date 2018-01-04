2221bf33f54e245c7bf6f16ca6d018ec0b5877b3
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2018-01-04 11:07:42 +0700

    Fix indentation

--- frontend/src/js/components/Form.js
+++ frontend/src/js/components/Form.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import request from 'superagent';
 
 class Form extends Component {
-    shortenUrl = (e) => {
-      e.preventDefault();
+  shortenUrl = (e) => {
+    e.preventDefault();
 
-      request.post('/generate')
-        .send({ Url: this.refs.url.value })
-        .end((err, res) => {
-          if (err) return alert(err);
+    request.post('/generate')
+      .send({Url: this.refs.url.value})
+      .end((err, res) => {
+        if (err) return alert(err);
 
-          const payload = JSON.parse(res.text);
-          this.props.setUrl(payload.Url);
-        });
-    };
+        const payload = JSON.parse(res.text);
+        this.props.setUrl(payload.Url);
+      });
+  };
 
-    render() {
-        return (
-            <form onSubmit={this.shortenUrl} className="App-form">
-                <input type="url" ref="url"/>
-                <br/>
-                <input type="submit" value="Shorten"/>
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form onSubmit={this.shortenUrl} className="App-form">
+        <input type="url" ref="url"/>
+        <br/>
+        <input type="submit" value="Shorten"/>
+      </form>
+    );
+  }
 }
 
 export default Form;
