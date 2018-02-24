2b25c6379da95400df56327ee7193452bbd9b886
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2018-02-24 12:11:03 -0500

    E2e tests running with webdriverio

---
+++ frontend/test/e2e/app.spec.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const getConfig = require('../../config');
+const config = getConfig(process.env.NODE_ENV);
+
+describe('shawty', function() {
+  beforeEach(function () {
+    browser.url('http://localhost:9001');
+  });
+
+  it('renders app', async function() {
+    const res = await browser.element('.App');
+    expect(res.state).to.equal('success');
+  });
+
+  it('submits link and receives code', async function() {
+    browser.setValue('input[type="uri"]', 'https://google.com');
+    browser.click('input[type="submit"]');
+    browser.waitForText('.App-result', 1000);
+    const text = browser.getText('.App-result');
+    expect(text).to.have.length(config.staticEndpoint.length + 6);
+  });
+});
