d71e1a0df26bd765ea66ee088e19e1d3d37644bb
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2018-03-10 15:49:47 -0500

    Webpack prod config and CORS setup for express

--- frontend/index.js
+++ frontend/index.js
@@ -9,12 +9,18 @@ const config = getConfig(process.env.NODE_ENV);
 // Serve all static assets normally
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Serve react app on requests to root
-app.get('/', function (request, response) {
+app.get('/', function(request, response) {
   response.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
-app.get('/:code', function (request, response) {
+app.get('/:code', function(request, response) {
   response.redirect(`${config.apiEndpoint}/${request.params.code}`);
 });
 
