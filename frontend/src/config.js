ae8a1d09dbf71f8a9ed8d80d91ca9b6b7aeae460
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2018-03-10 15:42:11 -0500

    Update production config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -4,7 +4,13 @@ const getConfig = (env) => {
       apiEndpoint: 'http://localhost:8080',
       staticEndpoint: 'http://localhost:9001',
     };
+  } else {
+    return {
+      apiEndpoint: 'http://104.197.107.231:8080',
+      staticEndpoint: 'http://35.226.201.59:9001',
+    }
   }
+
 };
 
 module.exports = getConfig;
